555dd33b40484cbcbd0967a9936fe306da8dce14
Shloak Sinha <ssinha8@ucsc.edu>
2023-12-12 19:19:03 -0800

    pushing the graves and merchant logic for collision and display

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,12 @@ context.fillRect(canvasPos, canvasPos, canvas.width, canvas.height);
 app.append(canvas);
 const playerImg = new Image();
 playerImg.src = "player.png";
+const merchantImg = new Image();
+merchantImg.src = "player.png";
 const ghostImg = new Image();
 ghostImg.src = "ghost.png";
+const graveImg = new Image();
+graveImg.src = "grave.png";
 const playerSpd = 10;
 const ghostSpd = 5;
 const keysPressed: Set<string> = new Set();
@@ -22,16 +26,66 @@ class Player {
   posX: number;
   posY: number;
   isAlive: boolean;
+  hasItem: number;
   constructor() {
     this.posX = 0;
     this.posY = 0;
     this.isAlive = true;
+    this.hasItem = 0;
   }
   addPlayer() {
     playerImg.onload = () => this.update();
   }
   update() {
     context.drawImage(playerImg, this.posX, this.posY);
+  }
+}
+
+class Merchant {
+  posX: number;
+  posY: number;
+  constructor() {
+    this.posX = 610;
+    this.posY = 600;
+  }
+  addMerchant() {
+    merchantImg.onload = () => this.update();
+  }
+  update() {
+    context.drawImage(merchantImg, this.posX, this.posY);
+  }
+  playerCheck(thisPlayer: Player) {
+    if (
+      thisPlayer.posX >= this.posX - 30 &&
+      thisPlayer.posX <= this.posX + 60 &&
+      thisPlayer.posY >= this.posY - 39 &&
+      thisPlayer.posY <= this.posY + 65 &&
+      thisPlayer.hasItem != 0
+    ) {
+      let text;
+      // FLAVOR TEXT IS THE TEXT BELOW FOR EACH ITEM!
+      switch (thisPlayer.hasItem) {
+        case 1:
+          text = "Gave item 1";
+          break;
+        case 2:
+          text = "gave item 2";
+          break;
+        case 3:
+          text = "gave item 3";
+          break;
+        case 4:
+          text = "gave item 4";
+      }
+      context.fillStyle = "black";
+      context.font = "64px bold Arial";
+      context.fillText(
+        `${text}`,
+        (canvas.width - 300) / 2,
+        (canvas.height - 60) / 2,
+      );
+      thisPlayer.hasItem = 0;
+    }
   }
 }
 
@@ -109,12 +163,44 @@ class Ghost {
   }
 }
 
+class Grave {
+  posX: number;
+  posY: number;
+  constructor(x: number, y: number) {
+    this.posX = x;
+    this.posY = y;
+  }
+  addGrave() {
+    graveImg.onload = this.update;
+  }
+  update() {
+    context.drawImage(graveImg, this.posX, this.posY);
+  }
+  playerCheck(thisPlayer: Player) {
+    if (
+      thisPlayer.posX >= this.posX + 100 &&
+      thisPlayer.posX <= this.posX + 170 &&
+      thisPlayer.posY >= this.posY + 50 &&
+      thisPlayer.posY <= this.posY + 120 &&
+      thisPlayer.hasItem == 0
+    ) {
+      thisPlayer.hasItem = Math.floor(Math.random() * 4) + 1;
+    }
+  }
+}
+
 function redrawCanvas() {
   if (currPlayer.isAlive) {
     context.fillStyle = "#FFE5B4";
     context.clearRect(canvasPos, canvasPos, canvas.width, canvas.height);
     context.fillRect(canvasPos, canvasPos, canvas.width, canvas.height);
+    allGraves.forEach((currGrave) => {
+      currGrave.update();
+      currGrave.playerCheck(currPlayer);
+    });
     currPlayer.update();
+    currMerchant.update();
+    currMerchant.playerCheck(currPlayer);
     allGhosts.forEach((currGhost) => {
       currGhost.update();
       currGhost.playerCheck(currPlayer);
@@ -123,6 +209,8 @@ function redrawCanvas() {
 }
 
 const currPlayer = new Player();
+const currMerchant = new Merchant();
+currMerchant.addMerchant();
 currPlayer.addPlayer();
 const allGhosts: Ghost[] = [];
 for (let i = 0; i < 4; i++) {
@@ -131,6 +219,24 @@ for (let i = 0; i < 4; i++) {
 }
 allGhosts.forEach((currGhost) => {
   currGhost.addGhost();
+});
+
+const GravePadding = 100;
+const allGraves: Grave[] = [];
+for (let i = 0; i < 4; i++) {
+  const currGrave = new Grave(GravePadding + i * 270, 150);
+  allGraves.push(currGrave);
+}
+allGraves.forEach((currGrave) => {
+  currGrave.addGrave();
+});
+
+for (let i = 0; i < 4; i++) {
+  const currGrave = new Grave(GravePadding + i * 270, 300);
+  allGraves.push(currGrave);
+}
+allGraves.forEach((currGrave) => {
+  currGrave.addGrave();
 });
 
 window.addEventListener("keydown", (e) => {
